40c6052bdf26ba16d7ee8bea2e7b6feb0ba473ff
GoldenPavilion <rcorystine@gmail.com>
2022-02-08 18:03:04 -0500

    set initial state of false for isDark

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,10 @@ import itemData from "../data/items";
 import React, { useState } from 'react';
 
 function App() {
-
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
+  const [isDark, setIsDark] = useState(false);
+
   const appClass = false ? "App dark" : "App light"
 
   return (
